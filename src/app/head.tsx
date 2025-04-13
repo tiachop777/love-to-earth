aab5f9f02893101146b9bc4d9b6d03022ae1c195
tiachop777 <134785602+tiachop777@users.noreply.github.com>
2025-04-13 20:53:12 +0200

    Add custom favicon and head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+// src/app/head.tsx
+export default function Head() {
+    return (
+      <>
+        <title>LoveToEarth 🌍</title>
+        <meta name="description" content="Lan tỏa yêu thương đến Trái Đất 💚" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </>
+    );
+  }
+  
